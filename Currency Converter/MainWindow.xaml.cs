3630d0e06dc6f7d8c994f9e240792645adf371c4
IlyaDoronin <ryletd@gmail.com>
2022-10-30 22:16:41 +0300

    '1'

--- Currency Converter/MainWindow.xaml.cs
+++ Currency Converter/MainWindow.xaml.cs
@@ -1,56 +1,37 @@
-﻿
-using Microsoft.Win32;
-using System;
-using System.Diagnostics;
-using System.Net;
-using System.Text.RegularExpressions;
-using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Currency_Converter
 {
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
-        //Создание экземпляров классов
-        public static Courses Page_Cources = new Courses();
-        public static Calcutator Page_Calcutator = new Calcutator();
-        public static Settings Page_Settings = new Settings();
-        public static AppSettings Page_AppSettings = new AppSettings();
         public MainWindow()
         {
             InitializeComponent();
-            Load();
         }
-        void Load()
-        {
-            try
-            {
-                if(Registry.CurrentUser.CreateSubKey("Software\\Currency converter").GetValue("LastPage").ToString() == "Calcutator")
-                    Page_Loader.Navigate(Page_Calcutator);
-                else
-                    Page_Loader.Navigate(Page_Cources);
-            }
-            catch
-            {
-                Page_Loader.Navigate(Page_Cources);
-            }
-        }
-        void TrayOff()
-        {
-            Show();
-            Tray.Visibility = Visibility.Hidden;
-        }
-        
-        void MainForm_Activated(object sender, EventArgs e)
-        {
-        }
+
         private void DragForm(object sender, MouseButtonEventArgs e)
         {
+
             try
-            { DragMove(); }
+            {
+                DragMove();
+            }
             catch { }
         }
 
@@ -58,47 +39,31 @@ namespace Currency_Converter
         {
             Close();
         }
+        private void FullScreen(object sender, RoutedEventArgs e)
+        {
+            if(MainForm.WindowState == WindowState.Maximized)
+            {
+                MainForm.Height = 750;
+                MainForm.Width = 550;
+            }
+            else
+            {
+                MainForm.WindowState = WindowState.Maximized;
+            }
+        }
         private void Minimized(object sender, RoutedEventArgs e)
         {
             MainForm.WindowState = WindowState.Minimized;
         }
-
-        // Сворачивание в трей Hardcodet.Wpf.TaskbarNotification
-        void TrayMinimized(object sender, RoutedEventArgs e)
+        private void TrayMinimized(object sender, RoutedEventArgs e)
         {
-            Hide();
-            Tray.Visibility = Visibility.Visible;
         }
-        void TaskbarIcon_TrayLeftMouseDown(object sender, RoutedEventArgs e)
+        private void Settings(object sender, RoutedEventArgs e)
         {
-            TrayOff();
         }
-        void Settings(object sender, RoutedEventArgs e)
+        
+        private void CloseForm_MouseEnter(object sender, MouseEventArgs e)
         {
-            TrayOff();
-            Page_Loader.Navigate(Page_Settings);
-        }
-
-        void Course_Click(object sender, RoutedEventArgs e)
-        {
-            TrayOff();
-            Page_Loader.Navigate(Page_Cources);
-        }
-        void Calcutator_Click(object sender, RoutedEventArgs e)
-        {
-            TrayOff();
-            Page_Loader.Navigate(Page_Calcutator);
-        }
-
-        void AppSettings(object sender, RoutedEventArgs e)
-        {
-            TrayOff();
-            Page_Loader.Navigate(Page_AppSettings);
-        }
-
-        void MainForm_Closed(object sender, EventArgs e)
-        {
-            Process.GetCurrentProcess().Kill();
         }
     }
 }
